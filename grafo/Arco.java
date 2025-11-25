2aa1eaf703ee4795e326cf3e7e166769daa6735c
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2025-11-25 17:48:13 +0000

    iniciando con grafo

---
+++ grafo/Arco.java
@@ -0,0 +1,45 @@
+package grafo;
+
+public class Arco {
+    //Atributos
+    //nombre de vertice destino
+    private String destino;
+    //perso del arco
+    private int peso;
+
+    //constructores
+    public Arco(String destino) {
+        this.destino = destino;
+    }
+    public Arco(String destino, int peso) {
+        this.destino = destino;
+        this.peso = peso;
+    }
+    //getter y setter
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        Arco a = (Arco) obj;
+        return this.destino == a.destino;
+        
+    } 
+    
+        
+    
+    
+    
+}
---
+++ grafo/Vertice.java
@@ -0,0 +1,24 @@
+package grafo;
+
+public class Vertice {
+    private String nombre;
+
+    public Vertice(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return  nombre ;
+    }
+    
+    
+}
